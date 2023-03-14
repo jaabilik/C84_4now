f40f43fea6ae8b158b1d23b0fd1912dd73aef97f
jaabilik <121368312+jaabilik@users.noreply.github.com>
2023-03-13 18:10:35 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -20,20 +20,41 @@ function uploadimg() {
 	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
 }
 
+window.addEventListener("keydown", my_keydown);
 function my_keydown(e)
 {
 	keyPressed = e.keyCode;
 	console.log(keyPressed);
 	
-		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90)) {
+	if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+		{
 			alphabetkey();
 			document.getElementById("d1").innerHTML="You pressed Alphabet Key";
 			console.log("alphabet key");
 		}
-	else {
+		else if(keyPressed >=48 && keyPressed<=57)
+		{
+			numberkey();
+			document.getElementById("d1").innerHTML="You pressed Number Key";
+			console.log("Number key");
+		}
+		else if(keyPressed >=37 && keyPressed<=40)
+		{
+			arrowkey();
+			document.getElementById("d1").innerHTML="You pressed Arrow Key";
+			console.log("Arrow Key");
+		}
+		else if((keyPressed ==17)|| (keyPressed ==18 || keyPressed ==27))
+		{
+			specialkey();
+			document.getElementById("d1").innerHTML="You pressed ctrl/esc/alt";
+			console.log("special key");
+		}
+	else{
 		otherkey();
 		document.getElementById("d1").innerHTML="You pressed symbol or other key";
 	}
+
 }
 
 function alphabetkey() {
